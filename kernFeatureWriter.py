9c4d77cb366b5c49630b17c376e53415ad65934b
Frank Grießhammer <fgriessh@adobe.com>
2017-05-16 21:41:08 -0700

    The copyright string is part of the repository.

--- kernFeatureWriter.py
+++ kernFeatureWriter.py
@@ -38,30 +38,6 @@ tag_RTL = '_RTL'
 tag_exception = 'EXC_'
 tag_ignore = '.cxt'
 
-###################################################
-
-__copyright__ = __license__ = '''
-Copyright (c) 2006-2016 Adobe Systems Incorporated. All rights reserved.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so,subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-'''
-
 __doc__ = '''
 kernFeatureWriter.py 1.0 - Sept 2016
 
@@ -1002,8 +978,6 @@ if __name__ == '__main__':
         '-x', action='store_true', help='test args')
 
     args = parser.parse_args()
-    # print args
-
     if args.test:
 
         import doctest
