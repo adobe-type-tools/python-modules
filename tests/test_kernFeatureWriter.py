d423a5c1dabdc9df1d40a436d07e0259938dc8a9
Frank Grießhammer <fgriessh@adobe.com>
2021-02-03 01:57:48 +0100

    Add simple tests

---
+++ tests/test_kernFeatureWriter.py
@@ -0,0 +1,32 @@
+import defcon
+import sys
+sys.path.append("..")
+
+from kernFeatureWriter import *
+
+
+def read_file(path):
+    '''
+    Read a file, split lines into a list, close the file.
+    '''
+
+    with open(path, 'r', encoding='utf-8') as f:
+        data = f.read().splitlines()
+    return data
+
+
+def test_WhichApp():
+    assert WhichApp().appName == 'Defcon'
+    # import __mocks__ as flsys ???
+    # assert WhichApp().appName == 'FontLab'
+
+
+def test_full_run():
+    args = Defaults()
+    test_dir = os.path.dirname(__file__)
+    ufo_path = os.path.join(test_dir, 'example.ufo')
+    example_feature = read_file(os.path.join(test_dir, 'example.fea'))
+    args.input_file = ufo_path
+    f = defcon.Font(ufo_path)
+    run(f, args)
+    assert read_file(os.path.join(test_dir, 'kern.fea')) == example_feature
