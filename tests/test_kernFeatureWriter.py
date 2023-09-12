99614e1b937d0c38198d39ea67f80c4093dae517
Frank Grießhammer <fgriessh@adobe.com>
2023-09-12 13:41:57 +0200

    use pathlib

--- tests/test_kernFeatureWriter.py
+++ tests/test_kernFeatureWriter.py
@@ -1,8 +1,11 @@
 import defcon
 import sys
-sys.path.append("..")
+from pathlib import Path
 
+sys.path.append("..")
 from kernFeatureWriter import *
+
+TEST_DIR = Path(__file__).parent
 
 
 def read_file(path):
@@ -23,38 +26,39 @@ def test_WhichApp():
 
 def test_full_run():
     args = Defaults()
-    test_dir = os.path.dirname(__file__)
-    ufo_path = os.path.join(test_dir, 'example.ufo')
-    example_feature = read_file(os.path.join(test_dir, 'example.fea'))
+    ufo_path = TEST_DIR / 'example.ufo'
+    example_feature = read_file(TEST_DIR / 'example.fea')
     args.input_file = ufo_path
     f = defcon.Font(ufo_path)
     run(f, args)
-    assert read_file(os.path.join(test_dir, 'kern.fea')) == example_feature
+    assert read_file(TEST_DIR / 'kern.fea') == example_feature
 
 
 def test_subtable():
     args = Defaults()
-    test_dir = os.path.dirname(__file__)
-    ufo_path = os.path.join(test_dir, 'example.ufo')
-    example_feature = read_file(os.path.join(test_dir, 'example_subs.fea'))
+    ufo_path = TEST_DIR / 'example.ufo'
+    example_feature = read_file(TEST_DIR / 'example_subs.fea')
     args.input_file = ufo_path
     args.write_subtables = True
     args.subtable_size = 128
-    args.output_file = 'temp_subs.fea'
+    output_file = TEST_DIR / 'temp_subs.fea'
+    args.output_file = output_file
     f = defcon.Font(ufo_path)
     run(f, args)
     assert (
-        read_file(os.path.join(test_dir, args.output_file)) == example_feature)
+        read_file(TEST_DIR / args.output_file)) == example_feature
+    output_file.unlink()
 
 
 def test_case_01():
     args = Defaults()
-    test_dir = os.path.dirname(__file__)
-    ufo_path = os.path.join(test_dir, 'case_01.ufo')
-    example_feature = read_file(os.path.join(test_dir, 'case_01.fea'))
+    ufo_path = TEST_DIR / 'case_01.ufo'
+    example_feature = read_file(TEST_DIR / 'case_01.fea')
     args.input_file = ufo_path
-    args.output_file = 'temp_case_01.fea'
+    output_file = TEST_DIR / 'temp_case_01.fea'
+    args.output_file = output_file
     f = defcon.Font(ufo_path)
     run(f, args)
     assert (
-        read_file(os.path.join(test_dir, args.output_file)) == example_feature)
+        read_file(TEST_DIR / args.output_file)) == example_feature
+    output_file.unlink()
