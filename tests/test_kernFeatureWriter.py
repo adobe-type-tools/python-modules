c13920741d88ed1b95e1e19850f19254ec611fc8
Frank Grießhammer <fgriessh@adobe.com>
2023-09-12 16:00:10 +0200

    add test for dissolving single-element groups

--- tests/test_kernFeatureWriter.py
+++ tests/test_kernFeatureWriter.py
@@ -34,46 +34,71 @@ def test_get_args():
 def test_full_run():
     args = Defaults()
     ufo_path = TEST_DIR / 'kern_example.ufo'
+    tmp_feature = TEST_DIR / 'tmp_kern_example.fea'
     example_feature = read_file(TEST_DIR / 'kern_example.fea')
     args.input_file = ufo_path
-    output_file = TEST_DIR / 'tmp_kern_example.fea'
-    args.output_file = output_file
+    args.output_file = tmp_feature
     f = defcon.Font(ufo_path)
     run(f, args)
-    assert read_file(TEST_DIR / args.output_file) == example_feature
-    output_file.unlink()
+    assert read_file(tmp_feature) == example_feature
+    tmp_feature.unlink()
 
 
 def test_subtable():
+    '''
+    test writing a file with subtable breaks
+    '''
     args = Defaults()
     ufo_path = TEST_DIR / 'kern_example.ufo'
+    tmp_feature = TEST_DIR / 'tmp_kern_example_subs.fea'
     example_feature = read_file(TEST_DIR / 'kern_example_subs.fea')
     args.input_file = ufo_path
     args.write_subtables = True
     args.subtable_size = 128
-    output_file = TEST_DIR / 'tmp_kern_example_subs.fea'
-    args.output_file = output_file
+    args.output_file = tmp_feature
     f = defcon.Font(ufo_path)
     run(f, args)
-    assert (
-        read_file(TEST_DIR / args.output_file)) == example_feature
-    output_file.unlink()
+    assert read_file(tmp_feature) == example_feature
+    tmp_feature.unlink()
+
+
+def test_dissolve():
+    '''
+    test dissolving single-glyph groups
+    '''
+    args = Defaults()
+    ufo_path = TEST_DIR / 'kern_AV.ufo'
+    tmp_feature_undissolved = TEST_DIR / 'tmp_kern_AV_undissolved.fea'
+    tmp_feature_dissolved = TEST_DIR / 'tmp_kern_AV_dissolved.fea'
+    example_feature_undissolved = read_file(
+        TEST_DIR / 'kern_AV_undissolved.fea')
+    example_feature_dissolved = read_file(
+        TEST_DIR / 'kern_AV_dissolved.fea')
+    args.input_file = ufo_path
+    args.output_file = tmp_feature_undissolved
+    f = defcon.Font(ufo_path)
+    run(f, args)
+    assert read_file(tmp_feature_undissolved) == example_feature_undissolved
+    args.dissolve_single = True
+    args.output_file = tmp_feature_dissolved
+    run(f, args)
+    assert read_file(tmp_feature_dissolved) == example_feature_dissolved
+    tmp_feature_undissolved.unlink()
+    tmp_feature_dissolved.unlink()
 
 
 def test_case_01():
     '''
-    This case tests a single member of a left-side group (Adieresis for the
-    A-group, Oslash for the O-group) having a kerning exception to a
-    right-side item.
+    test a kerning exception of a single member of a left-side group
+    (Adieresis for the A-group, Oslash for the O-group) to a right-side item.
     '''
     args = Defaults()
     ufo_path = TEST_DIR / 'kern_case_01.ufo'
+    tmp_feature = TEST_DIR / 'tmp_case_01.fea'
     example_feature = read_file(TEST_DIR / 'kern_case_01.fea')
     args.input_file = ufo_path
-    output_file = TEST_DIR / 'tmp_case_01.fea'
-    args.output_file = output_file
+    args.output_file = tmp_feature
     f = defcon.Font(ufo_path)
     run(f, args)
-    assert (
-        read_file(TEST_DIR / args.output_file)) == example_feature
-    output_file.unlink()
+    assert read_file(tmp_feature) == example_feature
+    tmp_feature.unlink()
