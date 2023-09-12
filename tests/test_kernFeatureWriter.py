e193c0b9bc18f8e8722a033c10bd4741ee6c2479
Frank Grießhammer <fgriessh@adobe.com>
2023-09-12 15:04:55 +0200

    clean up kfw tests

--- tests/test_kernFeatureWriter.py
+++ tests/test_kernFeatureWriter.py
@@ -33,22 +33,25 @@ def test_get_args():
 
 def test_full_run():
     args = Defaults()
-    ufo_path = TEST_DIR / 'example.ufo'
-    example_feature = read_file(TEST_DIR / 'example.fea')
+    ufo_path = TEST_DIR / 'kern_example.ufo'
+    example_feature = read_file(TEST_DIR / 'kern_example.fea')
     args.input_file = ufo_path
+    output_file = TEST_DIR / 'tmp_kern_example.fea'
+    args.output_file = output_file
     f = defcon.Font(ufo_path)
     run(f, args)
-    assert read_file(TEST_DIR / 'kern.fea') == example_feature
+    assert read_file(TEST_DIR / args.output_file) == example_feature
+    output_file.unlink()
 
 
 def test_subtable():
     args = Defaults()
-    ufo_path = TEST_DIR / 'example.ufo'
-    example_feature = read_file(TEST_DIR / 'example_subs.fea')
+    ufo_path = TEST_DIR / 'kern_example.ufo'
+    example_feature = read_file(TEST_DIR / 'kern_example_subs.fea')
     args.input_file = ufo_path
     args.write_subtables = True
     args.subtable_size = 128
-    output_file = TEST_DIR / 'temp_subs.fea'
+    output_file = TEST_DIR / 'tmp_kern_example_subs.fea'
     args.output_file = output_file
     f = defcon.Font(ufo_path)
     run(f, args)
@@ -58,11 +61,16 @@ def test_subtable():
 
 
 def test_case_01():
+    '''
+    This case tests a single member of a left-side group (Adieresis for the
+    A-group, Oslash for the O-group) having a kerning exception to a
+    right-side item.
+    '''
     args = Defaults()
-    ufo_path = TEST_DIR / 'case_01.ufo'
-    example_feature = read_file(TEST_DIR / 'case_01.fea')
+    ufo_path = TEST_DIR / 'kern_case_01.ufo'
+    example_feature = read_file(TEST_DIR / 'kern_case_01.fea')
     args.input_file = ufo_path
-    output_file = TEST_DIR / 'temp_case_01.fea'
+    output_file = TEST_DIR / 'tmp_case_01.fea'
     args.output_file = output_file
     f = defcon.Font(ufo_path)
     run(f, args)
